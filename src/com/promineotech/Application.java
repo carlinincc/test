551a2b42ce36c479fe3812d683da14a4bcd5273c
Nathan <nathanccarlson@gmail.com>
2023-11-29 11:38:29 -0600

    first commit

---
+++ src/com/promineotech/Application.java
@@ -0,0 +1,26 @@
+package com.promineotech;
+
+public class Application {
+
+	public static void main(String[] args) {
+		int nathanAge = 38;
+		System.out.println(nathanAge);
+		boolean isAge38 = nathanAge == 38;
+		System.out.println(isAge38);
+		System.out.println(nathanAge == 40);
+		
+		nathanAge = nathanAge + 2;
+		System.out.println(nathanAge);
+		
+		
+		String firstName = "Nathan";
+		String lastName = "Carlson";
+		String fullName = firstName + " " + lastName;
+		
+		System.out.println(fullName);
+				
+				
+
+	}
+
+}
